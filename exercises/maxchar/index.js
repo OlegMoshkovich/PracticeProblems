5228522a4d941a59ab91f7a14bd11a90416f07a1
Oleg Moshkovich <oleg.mosh@gmail.com>
2018-10-31 10:50:57 -0400

    added another solution to the max character problem

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -7,26 +7,50 @@
 
 function maxChar(str) {
 let maxValue = 0;
+let maxCharacter;
+
 const chars = {};
-  for(char of str){
-    chars[char] = chars[char]+1 || 1
+
+for(char of str){
+  if(chars[char]){
+  chars[char] = chars[char]+1
+  }else{
+  chars[char] = 1
+}}
+
+for (char in chars){
+  if(chars[char]>maxValue){
+    maxValue = chars[char];
+    maxCharacter = char;
   }
-
-  Object.entries(chars).forEach(([key, value]) => {
-
-   if(maxValue<value){
-     maxValue = value
-   }
-   console.log("maxvalue",maxValue)
-   console.log(`key= ${key} value = ${value}`)
-})
-
-const character = Object.keys(chars).find(key => chars[key] === maxValue)
-console.log('the character is -- ' + character)
-
-return character
+}
+  console.log('maxcharacer',maxCharacter)
+  console.log('maxValue',maxValue )
+return maxCharacter
 }
 
-maxChar("idon'tknowwhatyouaretalking about")
+// function maxChar(str) {
+// let maxValue = 0;
+// const chars = {};
+//   for(char of str){
+//     chars[char] = chars[char]+1 || 1
+//   }
+//
+//   Object.entries(chars).forEach(([key, value]) => {
+//
+//    if(maxValue<value){
+//      maxValue = value
+//    }
+//    console.log("maxvalue",maxValue)
+//    console.log(`key= ${key} value = ${value}`)
+// })
+//
+// const character = Object.keys(chars).find(key => chars[key] === maxValue)
+// console.log('the character is -- ' + character)
+//
+// return character
+// }
+
+maxChar("dsakjdhsakjhjjjjjjjjsjjsjjsjjjj")
 
 module.exports = maxChar;
