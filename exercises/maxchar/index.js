55cf710316e452913a260d4172f4075a938df5f1
Oleg Moshkovich <oleg.mosh@gmail.com>
2018-10-31 10:01:49 -0400

    added custom solution to the maximum character problem - involves objects

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,28 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+let maxValue = 0;
+const chars = {};
+  for(char of str){
+    chars[char] = chars[char]+1 || 1
+  }
+
+  Object.entries(chars).forEach(([key, value]) => {
+
+   if(maxValue<value){
+     maxValue = value
+   }
+   console.log("maxvalue",maxValue)
+   console.log(`key= ${key} value = ${value}`)
+})
+
+const character = Object.keys(chars).find(key => chars[key] === maxValue)
+console.log('the character is -- ' + character)
+
+return character
+}
+
+maxChar("idon'tknowwhatyouaretalking about")
 
 module.exports = maxChar;
