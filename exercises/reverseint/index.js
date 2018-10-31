c3286be7346be2d366b6e153cf543e808398fcd5
Oleg Moshkovich <oleg.mosh@gmail.com>
2018-10-31 08:54:31 -0400

    completed reverse int

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,17 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+let newNum = 0;
+
+if(Math.sign(n) === -1){
+newNum = n.toString().split('').reverse().filter(char =>char != 0 && char != '-').join('') *(-1)
+}else{
+newNum = n.toString().split('').reverse().filter(char =>char != 0 && char != '-').join('')}
+console.log('new number '+ newNum)
+return newNum
+}
+
+reverseInt(874568374560000);
 
 module.exports = reverseInt;
