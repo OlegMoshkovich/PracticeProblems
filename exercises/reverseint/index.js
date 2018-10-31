fa2909905073ea47bfa57a3d0bddebf140ef34a6
Oleg Moshkovich <oleg.mosh@gmail.com>
2018-10-31 09:12:28 -0400

    alternative solution to the reverse int using Math.sign

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,17 +8,28 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {
-let newNum = 0;
 
-if(Math.sign(n) === -1){
-newNum = n.toString().split('').reverse().filter(char =>char != 0 && char != '-').join('') *(-1)
-}else{
-newNum = n.toString().split('').reverse().filter(char =>char != 0 && char != '-').join('')}
-console.log('new number '+ newNum)
-return newNum
+
+function reverseInt(n){
+  const newNumber =
+  parseInt(n.toString().split('').reverse().join(''))
+  console.log(newNumber*Math.sign(n))
+  return newNumber*Math.sign(n)
 }
 
-reverseInt(874568374560000);
+reverseInt(-1800);
+
+// function reverseInt(n) {
+// let newNum = 0;
+//
+// if(Math.sign(n) === -1){
+// newNum = n.toString().split('').reverse().filter(char =>char != 0 && char != '-').join('') *(-1)
+// }else{
+// newNum = n.toString().split('').reverse().filter(char =>char != 0 && char != '-').join('')}
+// console.log('new number '+ newNum)
+// return newNum
+// }
+//
+// reverseInt(874568374560000);
 
 module.exports = reverseInt;
