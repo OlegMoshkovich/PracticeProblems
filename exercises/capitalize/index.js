c6769f1b6cad0b7e75346628a8ab5186e3d8e851
Oleg Moshkovich <oleg.mosh@gmail.com>
2018-11-05 16:55:07 -0500

    capitalization problem solved with the slice method

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -8,28 +8,39 @@
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
 
-
-// Self generated convoluted solution using object maps
+// slightly better solution with out the use of object maps
 function capitalize(str) {
-  let newObj = {}
-  let newString = []
-  let capitalized = []
-  let index = 0
-  newString = str.split(' ')
-  for (word of newString){
-    newObj[index] = word;
-    index++;
+  const newStr  = str.split(' ');
+  let index = 0;
+  for(word of newStr){
+    newStr[index] =  word[0].toUpperCase() + word.slice(1)
+    index ++;
   }
-  for (word in newObj){
-    newObj[word] = newObj[word].charAt(0).toUpperCase() + newObj[word].slice(1)
-    capitalized.push(newObj[word])
-  }
-  const newStringJoined = capitalized.join(' ')
-  console.log(newStringJoined)
-
-
+   const capitalized = newStr.join(' ')
+  console.log(capitalized)
+  return capitalized
 
 }
+
+// Self generated convoluted solution using object maps
+// function capitalize(str) {
+//   let newObj = {}
+//   let newString = []
+//   let capitalized = []
+//   let index = 0
+//   newString = str.split(' ')
+//   for (word of newString){
+//     newObj[index] = word;
+//     index++;
+//   }
+//   for (word in newObj){
+//     newObj[word] = newObj[word].charAt(0).toUpperCase() + newObj[word].slice(1)
+//     capitalized.push(newObj[word])
+//   }
+//   const newStringJoined = capitalized.join(' ')
+//   console.log(newStringJoined)
+//   return newStringJoined
+// }
 
 capitalize("I am here again");
 
