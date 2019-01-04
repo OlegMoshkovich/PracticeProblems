e2bedc0a89c3566175f7e21a27d5516824474ec0
Oleg Moshkovich <oleg.mosh@gmail.com>
2019-01-04 07:09:50 -0500

    working on the stack

--- exercises/stack/index.js
+++ exercises/stack/index.js
@@ -15,17 +15,13 @@ class Stack {
     this.data = []
   }
 }
-
 Stack.prototype.push = function(record){
   this.data.push(record);
 }
-
 Stack.prototype.pop = function(){
   return this.data.pop();
 }
-
 Stack.prototype.peek = function(){
   return this.data[this.data.length-1];
 }
-
 module.exports = Stack;
